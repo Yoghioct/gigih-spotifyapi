427bc0d2243ba549a8e4170b5261a8c55faa7fe8
yoghioctopus <yoghio@std.unissula.ac.id>
2022-03-23 14:15:12 +0700

    homework module 2 part 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import './App.css';
+import data from "./components/data"
 import axios from 'axios';
 import {useEffect, useState} from 'react';
 
@@ -8,11 +10,15 @@ function App() {
   const REDIRECT_URI = "http://localhost:3000"
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
-
+  
   const [token, setToken] = useState("")
 
   const [searchKey, setSearchKey] = useState("")
-  const [artists, setArtists] = useState([])
+  const [artists, setArtists] = useState([])  
+    
+  const image = data.album.images[0].url
+  const title = data.name
+  const artist = data.album.artists[0].name
 
   useEffect(() => {
       const hash = window.location.hash
@@ -32,6 +38,10 @@ function App() {
   const logout = () => {
       setToken("")
       window.localStorage.removeItem("token")
+  }
+
+  const Song = () => {
+    <Song />
   }
 
   const searchArtists = async (e) => {
@@ -54,30 +64,54 @@ const renderArtists = () => {
       <div key={artist.id}>
           {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
           {artist.name}
+          <Song />
       </div>
   ))
 }
 
   return (
+
       <div className="App">
           <header className="App-header">
               <h1>Spotify React</h1>
+
+              
               {!token ?
                   <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
-                      to Spotify</a>
-                  : <button onClick={logout}>Logout</button>}
-      {token ? 
+                      to Spotify</a> 
+                   : <button onClick={logout}>Logout</button>
+                   }
+         {token ? 
+
+        // {!token ?
+        //     <a href={`${process.env.REACT_APP_AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=${process.env.REACT_APP_RESPONSE_TYPE}`}>Login
+        //         to Spotify</a>
+        //     : <button onClick={logout}>Logout</button>}
+        // {token ? 
+        
+        <div className="card"> 
+        <img src={image} alt="imageAlbum" id="imageAlbum" /> 
+        <h2 id="songTitle">{title}</h2>
+        <p className="title" id="artists">{artist}</p>
+        <button className="button">Select</button>   
+        
       <form onSubmit={searchArtists}>
           <input type="text" onChange={e => setSearchKey(e.target.value)}/>
           <button type={"submit"}>Search</button>
       </form>
-      : <h3>Please Login</h3>
+      </div>
+      : <h3>Please Login Terlebih Dahulu</h3>
+
       }
 {renderArtists()}
-
           </header>
       </div>
   );
 }
 
 export default App;
+
+
+
+
+ 
---
+++ src/components/song.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import data from "./data";
+
+// const image = data.album.images[0].url
+// const title = data.name
+// const artist = data.album.artists[0].name
+
+// class Song extends React.Component {
+//     render() {
+//         return (
+//             <div className="card"> 
+//     <img src={image} alt="imageAlbum" id="imageAlbum" /> 
+//     <h2 id="songTitle">{title}</h2>
+//     <p className="title" id="artists">{artist}</p>
+//     <button className="button">Select</button>
+//             </div>
+//         );
+//     }
+// }
+// export default Song;
